5cda36f7df2a625f580ac215aa58b24fa1e314f8
Axiks <secure@mail.com>
2022-03-24 10:10:01 +0200

    Add firebase xml and setting firebase

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -11,14 +11,67 @@ class LoginScreen extends StatefulWidget{
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-
   final currentState = MobileVerificationState.SHOW_MOBILE_FORM_STATE;
+
+  final phoneController = TextEditingController();
+  final otpController = TextEditingController();
+
+  getMobileFromWidget(BuildContext context) {
+    return Column(
+      children: [
+        Spacer(),
+        TextField(
+          controller: phoneController,
+          decoration: InputDecoration(
+            hintText: "Phone Number",
+          ),
+        ),
+        SizedBox(height: 16,),
+        FlatButton(
+            onPressed: (){
+
+            },
+            child: Text("Send"),
+          color: Colors.lightBlue,
+          textColor: Colors.white,
+        ),
+        Spacer()
+      ],
+    );
+  }
+
+  getOtpFromWidget(BuildContext context) {
+    return Column(
+      children: [
+        Spacer(),
+        TextField(
+          controller: otpController,
+          decoration: InputDecoration(
+            hintText: "Enter OTP",
+          ),
+        ),
+        SizedBox(height: 16,),
+        FlatButton(
+          onPressed: (){
+
+          },
+          child: Text("VERIFTY"),
+          color: Colors.lightBlue,
+          textColor: Colors.white,
+        ),
+        Spacer()
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Login Screen"),
+      body: Container(
+          child: currentState == MobileVerificationState.SHOW_MOBILE_FORM_STATE
+              ? getMobileFromWidget(context)
+              : getOtpFromWidget(context),
+        padding: const EdgeInsets.all(16),
       ),
     );
   }
